06805d915b0ed234ce3f214bb6967754eee60dab
vance.petitt <vance.petitt@gmail.com>
2022-04-08 10:53:47 -0500

    Complete CRUD functionality for all apps, sucessfully tested. Added postman collection to local project folder.

--- backend/drf_jwt_backend/materials/serializers.py
+++ backend/drf_jwt_backend/materials/serializers.py
@@ -6,5 +6,7 @@ from .models import Materials
 class MaterialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Materials
-        fields = ['designation', 'family']
+        fields = ['designation', 'family', 'family_id']
         depth = 1 #allows for display of family information
+
+    family_id = serializers.IntegerField(write_only=True)
--- backend/drf_jwt_backend/test_data/serializers.py
+++ backend/drf_jwt_backend/test_data/serializers.py
@@ -6,8 +6,10 @@ from .models import TestData
 class TestDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestData
-        fields = ['environment', 'material', 'corrosion_rate', 'localized']
+        fields = ['environment', 'material', 'corrosion_rate', 'localized', 'environment_id', 'material_id']
         depth = 2 #need to be able to read --> material --> family from foreign keys
 
+    environment_id = serializers.IntegerField(write_only=True)
+    material_id = serializers.IntegerField(write_only=True)
 
 
