56c88d1fd0667bddf45a33258491f18b10e7519a
vance.petitt <vance.petitt@gmail.com>
2022-04-15 16:53:57 -0500

    Restored data that had been causing a breakage in the color functionality. Troubleshooting.

--- frontend/src/components/data_displays/FamilyDataDisplay/FamilyDataDisplay.jsx
+++ frontend/src/components/data_displays/FamilyDataDisplay/FamilyDataDisplay.jsx
@@ -6,22 +6,72 @@ import {Chart} from "react-google-charts";
 const FamilyDataDisplay = ({tests}) => {
     
     const {state} = useLocation();
+    
     const [familyForDisplay, setFamilyForDisplay] = useState(['']);
+    const [ratingFamily, setRatingFamily] = useState('');
+    const [ratingColorFamily, setRatingColorFamily] = useState('');
+    
+    // let rate = parseInt(state.test.corrosion_rate);
+    // let localized = state.test.localized;
 
+    function generateRating () {
+        
+        let rate = parseInt(state.test.corrosion_rate);
+        let localized = state.test.localized;
+        
+        let rating = '';
+        if (rate < 5 && localized === "none") {
+            ratingFamily = 'Recommended';
+        }
+        else if (rate < 5 && localized !== "none") {
+            ratingFamily = 'Recommended with caution';
+        }
+         else if (rate >= 20) {
+            ratingFamily = 'Not Recommended';
+        }
+        else if (rate < 20 && rate >= 5 && localized === "none") {
+            ratingFamily = 'Recommended with caution';
+        }
+        else if (localized.includes('severe')) {
+            ratingFamily = 'Not Recommended';
+        }
+        else {
+            ratingFamily = 'Recommended with caution';
+        };
+        console.log(ratingFamily);
+        setRatingFamily(ratingFamily);
+
+        let ratingColor = ''
+        if (ratingFamily === 'Recommended') {
+            ratingColorFamily = 'Green';
+        }
+        else if (ratingFamily === 'Recommended with caution') {
+            ratingColorFamily = 'Yellow';
+        }
+        else if (ratingFamily === 'Not Recommended') {
+            ratingColorFamily = 'Red';
+        };
+        console.log('color', ratingColor);
+        setRatingColorFamily(ratingColorFamily);
+    };
     let filteredTests = tests.filter(test => test.environment.id === state.test.environment.id && test.material.family.id === state.test.material.family.id)
     console.log('filtered', filteredTests)
 
     let array = filteredTests.map(test => {
-        filteredTests.forEach(test => {        
+        filteredTests.forEach(test => {
+            let rate = parseInt(state.test.corrosion_rate);
+            let localized = state.test.localized;
+            
+            return [test.material.designation, parseFloat(test.corrosion_rate), ratingColorFamily]      
             });
-    return [test.material.designation, parseFloat(test.corrosion_rate) ]
+    return [test.material.designation, parseFloat(test.corrosion_rate), ratingColorFamily ]
     });
     console.log('array', array)
 
     function generateFamilyChartData() {
         
         const data = [
-            ["material", "corrosion rate (mil/yr)", ],
+            ["material", "corrosion rate (mil/yr)", {role: "style"}],
             ...array
         ];
         console.log("family data", data)
