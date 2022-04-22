decf4757572eeaeb6731719c7ac74df135879b68
vance.petitt <vance.petitt@gmail.com>
2022-04-22 10:24:42 -0500

    Updated user to be input with new test data object. Allows for user traceability.;

--- backend/drf_jwt_backend/test_data/models.py
+++ backend/drf_jwt_backend/test_data/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from materials.models import Materials
 from environments.models import Environments
 from families.models import Family
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,4 +11,5 @@ class TestData(models.Model):
     material = models.ForeignKey(Materials, on_delete=models.CASCADE)
     corrosion_rate = models.DecimalField(max_digits=10, decimal_places=2)
     localized = models.CharField(max_length = 255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
 
